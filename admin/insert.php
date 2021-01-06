0ffa78c758b1aa5900a881d1bfae3738357b32c8
Gregory Caloin <gregchapauli@gmail.com>
2021-01-06 22:59:47 +0100

    Ajout de la fonction "Ajouter un item" part1

---
+++ admin/insert.php
@@ -0,0 +1,71 @@
+<?php
+    require 'database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Holtwood+One+SC&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <title>Burger Code</title>
+    </head>
+
+    <body>
+        <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
+        <div class="container admin">
+            <div class="row">
+                <h1><strong>Ajouter un item</strong></h1>
+                <br>
+                <form class="form" role="form" action="insert.php" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="name">Nom: </label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Nom" value="<?php echo $name; ?>">
+                        <span class="help-inline"><?php echo $nameError; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description: </label>
+                        <input type="text" class="form-control" id="description" name="description" placeholder="Description" value="<?php echo $description; ?>">
+                        <span class="help-inline"><?php echo $descriptionError; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="price">Prix: (en €) </label>
+                        <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Prix" value="<?php echo $price; ?>">
+                        <span class="help-inline"><?php echo $priceError; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Catégorie: </label>
+                        <select class="form-control" id="category" name="category">
+                            <?php 
+                                $db = Database::connect();
+                                foreach($db->query('SELECT * FROM categories') as $row)
+                                {
+                                    echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
+                                }
+                                $Database::disconnect();
+                            ?>
+                        </select>
+                        <span class="help-inline"><?php echo $categoryError; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="image">Sélectionner une image:</label>
+                        <input type="file" id="image" name="image">
+                        <span class="help-inline"><?php $imageError;?></span>
+                    </div>
+                </form>
+                <br>
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-pencil"></span> Ajouter</button>
+                    <a class="btn btn-primary" href="index.php"><span class="glyphicon glyphicon-arrow-left"></span> Retour</a>
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html>
--- admin/view.php
+++ admin/view.php
@@ -52,7 +52,7 @@
                             <label for="">Description: </label><?php echo ' ' . $item['description']; ?>
                         </div>
                         <div class="form-group">
-                            <label for="">Prix: </label><?php echo ' ' . number_format((float)$item['price'],2,'.',''); ?>
+                            <label for="">Prix: </label><?php echo ' ' . number_format((float)$item['price'],2,'.','') . ' €'; ?>
                         </div>
                         <div class="form-group">
                             <label for="">Catégorie: </label><?php echo ' ' . $item['category']; ?>
@@ -70,7 +70,7 @@
                 <div class="col-sm-6 site">
                     <div class="thumbnail">
                         <img src="<?php echo '../images/' . $item['image']; ?>" alt="...">
-                        <div class="price"><?php echo number_format((float)$item['price'],2,'.',''); ?></div>
+                        <div class="price"><?php echo number_format((float)$item['price'],2,'.','') . ' €'; ?></div>
                         <div class="caption">
                             <h4><?php echo $item['name']; ?></h4>
                             <p><?php echo $item['description']; ?></p>
